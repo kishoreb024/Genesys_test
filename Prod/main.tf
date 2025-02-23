4d6d5ab36f4c3a131a941a504576e8a13057bcb1
kishoreb024 <kishore.bhupal@gmail.com>
2025-02-23 23:00:53 +0530

    main commit

--- Prod/main.tf
+++ Prod/main.tf
@@ -15,10 +15,15 @@ terraform {
   }
 }
 
-
+variable "GENESYSCLOUD_OAUTHCLIENT_ID" {
+  default = ""
+}
+variable "GENESYSCLOUD_OAUTHCLIENT_SECRET" {
+  default = ""
+}
 
 provider "genesyscloud" {
-  oauthclient_id = "b9f8ef5f-397c-4624-8e3d-a7aaef53f68e"
-  oauthclient_secret = "svg1eWNtXVNj8BN-4BDQqBhnD3Jf5oY_q2DixDa0d-E"
+  oauthclient_id = var.GENESYSCLOUD_OAUTHCLIENT_ID
+  oauthclient_secret = var.GENESYSCLOUD_OAUTHCLIENT_SECRET
   aws_region = "ap-south-1"
 }
