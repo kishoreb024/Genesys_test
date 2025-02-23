44f85a449c1e678b73df2b768c19d8fa0eb66162
kishoreb024 <kishore.bhupal@gmail.com>
2025-02-23 16:42:57 +0530

    branch_dev commit

---
+++ Prod/New_Flow_import.tf
@@ -0,0 +1,30 @@
+resource "genesyscloud_flow" "New_Flow" {
+  filepath          = "AirIndiaNaresh_v37-0.yaml"
+  file_content_hash = filesha256("AirIndiaNaresh_v37-0.yaml")
+  // Example flow configuration using substitutions:
+  /*
+  inboundCall:
+    name: "{{flow_name}}"
+    defaultLanguage: "{{default_language}}"
+    startUpRef: ./menus/menu[mainMenu]
+    initialGreeting:
+      tts: "{{greeting}}"
+    menus:
+      - menu:
+          name: Main Menu
+          audio:
+            tts: You are at the Main Menu, press 9 to disconnect.
+          refId: mainMenu
+          choices:
+            - menuDisconnect:
+                name: "{{menu_disconnect_name}}"
+                dtmf: digit_9
+  */
+  // see https://developer.genesys.cloud/devapps/archy/flowAuthoring/lesson_07_substitutions
+  // these replace the key-value pairs from the --optionsFile when using the archy CLI
+  substitutions = {
+    flow_name            = "Sample TF Prod"
+    default_language     = "en-us"
+    greeting             = "Hello World"
+  }
+}
---
+++ Prod/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  cloud { 
+    
+    organization = "Genesys_Mumbai_Test" 
+
+    workspaces { 
+      name = "Genesys_Prod_TF" 
+    } 
+  } 
+  required_providers {
+    genesyscloud = {
+      source  = "mypurecloud/genesyscloud",
+      version = ">= 1.6.0"
+    }
+  }
+}
+
+provider "genesyscloud" {
+  oauthclient_id = "b9f8ef5f-397c-4624-8e3d-a7aaef53f68e"
+  oauthclient_secret = "svg1eWNtXVNj8BN-4BDQqBhnD3Jf5oY_q2DixDa0d-E"
+  aws_region = "ap-south-1"
+}
+
