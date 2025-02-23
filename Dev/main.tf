7e1cb26d91bf473be4040348cc124c66a837f479
kishoreb024 <kishore.bhupal@gmail.com>
2025-02-23 18:25:51 +0530

    main commit

--- Dev/main.tf
+++ Dev/main.tf
@@ -15,6 +15,24 @@ terraform {
   }
 }
 
+variable "GENESYSCLOUD_OAUTHCLIENT_ID" {
+  type        = string
+  description = "Genesys Client ID"
+  sensitive = true
+}
+
+variable "GENESYSCLOUD_OAUTHCLIENT_SECRET" {
+  type        = string
+  description = "Genesys Secret"
+  sensitive = true
+}
+
+variable "GENESYSCLOUD_REGION" {
+  type        = string
+  description = "Genesys Region"
+  sensitive = true
+}
+
 provider "genesyscloud" {
   oauthclient_id = GENESYSCLOUD_OAUTHCLIENT_ID
   oauthclient_secret = GENESYSCLOUD_OAUTHCLIENT_SECRET
