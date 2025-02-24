c2ab5f35758e47f82f3bbe5ce8b5d1f17f8fa185
kishoreb024 <kishore.bhupal@gmail.com>
2025-02-24 10:09:37 +0530

    main commit

--- Prod/New_Flow_import.tf
+++ Prod/New_Flow_import.tf
@@ -1,4 +1,4 @@
-resource "genesyscloud_flow" "Demo_Flow" {
+resource "genesyscloud_flow" "Demo_Prod_Flow" {
   filepath          = "Kishore_Terraform_Demo_Template_v2-0.yaml"
   file_content_hash = filesha256("Kishore_Terraform_Demo_Template_v2-0.yaml")
 
