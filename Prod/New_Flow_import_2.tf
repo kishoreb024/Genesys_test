8c093ce59307eabac87f6f0c43a7a586704c5589
kishoreb024 <kishore.bhupal@gmail.com>
2025-04-07 14:55:27 +0530

    main commit

--- Prod/New_Flow_import_2.tf
+++ Prod/New_Flow_import_2.tf
@@ -1,9 +1,9 @@
-resource "genesyscloud_flow" "New_Flow_2" {
+resource "genesyscloud_flow" "Test_Flow" {
   filepath          = "Kishore_Terraform_Demo_Template_v2-0.yaml"
   file_content_hash = filesha256("Kishore_Terraform_Demo_Template_v2-0.yaml")
 
   substitutions = {
-    flow_name            = "LBG_TF_Test_Prod"
+    flow_name            = "LBG_TF_Test_Prod_v2"
     default_language     = "en-us"
     greeting             = "Hello, Welcome to LBG"
   }
