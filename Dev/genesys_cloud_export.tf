4babf55313f19726373758a81093a9beb3e44de4
kishoreb024 <kishore.bhupal@gmail.com>
2025-05-17 09:53:21 +0530

    main commit

--- Dev/genesys_cloud_export.tf
+++ Dev/genesys_cloud_export.tf
@@ -2,13 +2,14 @@ resource "genesyscloud_tf_export" "export" {
   directory = "./"
 
   resource_types = [
-    #"genesyscloud_architect_schedules"
+    "genesyscloud_architect_schedules"
 	#"genesyscloud_routing_queue"
 	#"genesyscloud_flow"
-	"genesyscloud_architect_schedulegroups"
+	"genesyscloud_architect_schedules"
   ]
   ignore_cyclic_deps           = true
   split_files_by_resource      = true
   enable_dependency_resolution = true
-  export_format = "hcl"
+  export_as_hcl = true
+  include_state_file = true
 }
